dbf4e6f2cbe4166346208aafe0956bf6e86d543e
LeeadJ <leead123@gmail.com>
2022-03-15 14:15:00 +0200

    Finished Main

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,24 @@
 #include "mat.hpp"
 #include<iostream>
-#include<stdexcept>
-using namespace std;
+
+using namespace ariel;
 
 int main(){
-    cout << ariel::mat(5,5,'$','*');
+    //normal carpet
+    cout << "5x5 Carpet:" << endl;
+    cout << mat(5,5,'@','~') << endl;
+    cout << endl;
+    //row =1
+    cout << "5x1 Carpet:" << endl;
+    cout << mat(5,1,'#','~') << endl;
+    cout << endl;
+    //column = 1
+    cout << "1x5 Carpet:" << endl;
+    cout << mat(1,5,'#','~') << endl;
+    cout << endl;
+    //row and col = 1:
+    cout << "1x1 Carpet:" << endl;
+    cout << mat(1,1,'#','~') << endl;
+    cout << endl;
     return 0;
 }
